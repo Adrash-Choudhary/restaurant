a0adac2d7b8def1733a7bb81328ce05f601434a2
Adrash-Choudhary <128316267+Adrash-Choudhary@users.noreply.github.com>
2023-04-10 14:56:21 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -91,23 +91,7 @@ $result= mysqli_query($conn,$query);
 <div class="row g-4">
 <div class="col-12">
 
-<!--<div class="row gy-4">
-<div class="col-md-4">
-<h5 class="section-title ff-secondary fw-normal text-start text-primary">Booking</h5>
-<p><i class="fa fa-envelope-open text-primary me-2"></i>book@example.com</p>
-</div>
 
-<div class="col-md-4">
-<h5 class="section-title ff-secondary fw-normal text-start text-primary">General</h5>
-<p><i class="fa fa-envelope-open text-primary me-2"></i>info@example.com</p>
-</div>
-
-<div class="col-md-4">
-<h5 class="section-title ff-secondary fw-normal text-start text-primary">Technical</h5>
-<p><i class="fa fa-envelope-open text-primary me-2"></i>tech@example.com</p>
-</div>
-
-</div>  -->
 </div>
 <div class="col-md-6 wow fadeIn" data-wow-delay="0.1s">
 <iframe class="position-relative rounded w-100 h-100"
